3da376dcacd21cb7445e173ae76ccb1faf0d3679
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-08 19:00:51 +0300

    client upd + refactoring commit title upd

--- client/src/main/java/com/megateam/lab/client/Console.java
+++ client/src/main/java/com/megateam/lab/client/Console.java
@@ -1,8 +1,9 @@
 package com.megateam.lab.client;
 
-public class Console {
-
-  public static void main(String[] args) {
+public class Console
+{
+  public static void main(String[] args)
+  {
     System.out.println("works");
   }
 }
